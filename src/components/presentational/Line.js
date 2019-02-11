12e898ca9a1b916feabb60f090412c04812dcf25
ninabondar <nina.bondar.cs@gmail.com>
2019-02-11 19:50:45 +0200

    change line styles

--- src/components/presentational/Line.js
+++ src/components/presentational/Line.js
@@ -8,7 +8,7 @@ class Line extends React.Component {
     render () {
       const {x1,y1,x2,y2} = this.props;
       return (
-          <line x1={x1} y1={y1} x2={x2} y2={y2} />
+          <line x1={x1} y1={y1} x2={x2} y2={y2} style="stroke:rgb(255,0,0);stroke-width:2" />
       )
   }
 }
