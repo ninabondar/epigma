0517817fc3fd5d63d221552b2ebc8fb6acfd12c7
Mykhailo Ivankiv <neformal.lviv@gmail.com>
2019-02-13 00:54:15 +0200

    Edit multiple polygons.

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,85 +1,62 @@
 import React, { Component } from "react";
-import { init } from "ramda";
 import "../styles/canvas.scss";
-import { serializePath } from "../utils/helper";
 import Path from "./Path";
 
 class Canvas extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      mode: "view",
+      editIndex: null,
       objects: []
     };
   }
 
   componentDidMount() {
     const { x, y } = this.refs.svgRoot.getBoundingClientRect();
-
-    document.addEventListener("keydown", ev => {
-      const { key } = ev;
-      switch (key) {
-        case "Escape":
-        case "Enter": {
-          ev.preventDefault();
-          return this.endEdit();
-        }
-      }
-    });
-
-    this.setState({
-      offset: { x, y }
-    });
+    this.setState({ offset: { x, y } });
   }
 
-  drawPoint = ({ pageX: x, pageY: y }) => {
+  addPath = ({ pageX: x, pageY: y }) => {
     const { objects, offset } = this.state;
     const point = { x: x - offset.x, y: y - offset.y };
-    this.setState({
-      objects: objects.length === 0 ? [point, point] : [...objects, point]
-    });
-  };
-
-  editPoint = ({ pageX: x, pageY: y }) => {
-    const { objects, offset } = this.state;
-    const point = { x: x - offset.x, y: y - offset.y };
-    this.setState({
-      objects: [...init(objects), point]
-    });
-  };
-
-  endEdit = () => {
-    const { objects } = this.state;
-    this.setState({ mode: "view", objects: init(objects) });
-  };
-
-  startEdit = ({ pageX: x, pageY: y }) => {
-    const { offset } = this.state;
-    const point = { x: x - offset.x, y: y - offset.y };
 
     this.setState({
-      mode: "edit",
-      objects: [point, point]
+      objects: [...objects, [point, point]],
+      editIndex: objects.length
     });
   };
 
   render() {
-    const { objects, mode } = this.state;
+    const { objects, offset, editIndex } = this.state;
     return (
       <>
         <svg
           ref={"svgRoot"}
           className="canvas"
-          onMouseMove={
-            mode === "edit" && objects.length > 1 ? this.editPoint : null
-          }
-          onClick={mode === "edit" ? this.drawPoint : this.startEdit}
+          onClick={editIndex === null ? this.addPath : null}
         >
-          {objects.length >= 2 ? (
-            <path className={"canvas__path"} d={serializePath(objects)} />
-          ) : (
-            "Click to start drawing."
-          )}
+          {objects.length >= 1 &&
+            objects.map((path, index) => (
+              <Path
+                onSelect={() => this.setState({ editIndex: index })}
+                key={index}
+                edit={editIndex === index}
+                onChange={path =>
+                  this.setState({
+                    objects: [
+                      ...objects.slice(0, editIndex),
+                      path,
+                      ...objects.slice(editIndex + 1)
+                    ],
+                    editIndex: null
+                  })
+                }
+                offset={offset}
+                path={path}
+              />
+            ))}
+
+          {objects.length < 0 && <text dy={20}>Click to start drawing.</text>}
         </svg>
       </>
     );
--- src/components/Path.js
+++ src/components/Path.js
@@ -1,11 +1,94 @@
 import React, { Component } from "react";
 import { serializePath } from "../utils/helper";
+import { init } from "ramda";
 
-class Path extends Component {
+const PathView = ({ path, onSelect }) => (
+  <g
+    onClick={ev => {
+      ev.preventDefault();
+      ev.stopPropagation();
+      return onSelect();
+    }}
+  >
+    <path className={"canvas__path"} d={serializePath(path)} />
+  </g>
+);
+
+class PathEdit extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      path: this.props.path
+    };
+  }
+
+  componentWillReceiveProps(nextProps, nextContext) {
+    this.setState({ path: nextProps.path });
+  }
+
+  keyHandler = ev => {
+    const { key } = ev;
+    switch (key) {
+      case "Escape":
+      case "Enter": {
+        ev.preventDefault();
+        return this.endEdit();
+      }
+    }
+  };
+
+  componentDidMount() {
+    document.addEventListener("keydown", this.keyHandler);
+    document.addEventListener("mousemove", this.editPoint);
+    document.addEventListener("click", this.drawPoint);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.keyHandler);
+    document.removeEventListener("mousemove", this.editPoint);
+    document.removeEventListener("click", this.drawPoint);
+  }
+
+  drawPoint = ({ pageX: x, pageY: y }) => {
+    const { path } = this.state;
+    const { offset } = this.props;
+
+    const point = { x: x - offset.x, y: y - offset.y };
+
+    this.setState({
+      path: path.length === 0 ? [point, point] : [...path, point]
+    });
+  };
+
+  editPoint = ({ pageX: x, pageY: y }) => {
+    const { path } = this.state;
+    const { offset } = this.props;
+
+    const point = { x: x - offset.x, y: y - offset.y };
+
+    this.setState({
+      path: [...init(path), point]
+    });
+  };
+
+  endEdit = () => {
+    const { path } = this.state;
+    this.props.onChange(init(path));
+  };
+
   render() {
-    const { path } = this.props;
-    return <path d={serializePath(path)} />;
+    const { path } = this.state;
+    return (
+      <g>
+        <path className={"canvas__path"} d={serializePath(path)} />
+        {path.map(({ x, y }, index) => (
+          <circle key={index} cx={x} cy={y} r={4} />
+        ))}
+      </g>
+    );
   }
 }
 
-export default Path;
+export default ({ edit, ...props }) => {
+  return edit ? <PathEdit {...props} /> : <PathView {...props} />;
+};
