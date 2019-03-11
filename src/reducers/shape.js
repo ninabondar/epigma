94f7dc98b621b2c59fa8ea1891f2f2d33fbdb1fc
Nina Bondar <nina.bondar.cs@gmail.com>
2019-03-11 22:42:21 +0200

    Fix hardcode in configureStore.

--- src/reducers/shape.js
+++ src/reducers/shape.js
@@ -1,5 +1,35 @@
-const defaultState = {};
-
+const defaultState = [
+  {
+    id: 1,
+    points: [
+      {
+        x: 139,
+        y: 157.3333282470703
+      },
+      {
+        x: 424,
+        y: 294.3333282470703
+      },
+      {
+        x: 443,
+        y: 42.33332824707031
+      },
+      {
+        x: 186,
+        y: 251.3333282470703
+      },
+      {
+        x: 628,
+        y: 174.3333282470703
+      },
+      {
+        x: 141,
+        y: 155.3333282470703
+      }
+    ],
+    style: null
+  }
+];
 export const setEditIndex = (state = defaultState, action) => {
   return {
     ...state,
